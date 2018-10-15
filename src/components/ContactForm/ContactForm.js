58acab4bad8d782677405a25646b84cf66fed0de
kruzyk <kruzyk@gmail.com>
2018-10-15 23:47:37 +0200

    start ContactForm component

---
+++ src/components/ContactForm/ContactForm.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react'
+
+const initialState = {
+    contactName: '',
+    contactSurname: '',
+    contactNumber: '',
+    errors: {}
+}
+
+class ContactForm extends Component{
+  
+    render(){
+        return(
+
+        )
+    }
+}
+
+export default ContactForm
